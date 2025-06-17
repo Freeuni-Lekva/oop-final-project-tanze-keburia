1c8e914ae98fedeb1f2c51907ce6d52f9134fa22
icosahedron31 <tdola23@freeuni.edu.ge>
2025-06-18 00:28:39 +0400

    implemented login/signup servlets

--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -3,6 +3,7 @@ package servlets;
 import classes.DAOConnecter;
 import classes.UserDAO;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,23 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
-@WebServlet("/LoginServlet")
+@WebServlet("LoginServlet")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        try(Connection conn = DAOConnecter.getConnection()){
-            UserDAO dao = new UserDAO(conn);
-           HttpSession session = request.getSession();
-          session.setAttribute("username", username);
-            if(dao.userExists(username) && dao.checkPassword(username, password)){
-                response.sendRedirect("index.jsp");
-            } else {
-                response.sendRedirect("loginFailed.jsp");
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
+        ServletContext context = getServletContext();
+        UserDAO userDAO = (UserDAO)context.getAttribute("users");
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+        if(userDAO.userExists(username) && userDAO.checkPassword(username, password)){
+            response.sendRedirect("index.jsp");
+        } else {
+            response.sendRedirect("loginFailed.jsp");
         }
+
     }
 }
