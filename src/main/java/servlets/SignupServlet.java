e60c892742b44f2dfb6659bc8cd33673d373cfac
icosahedron31 <tdola23@freeuni.edu.ge>
2025-06-18 01:01:19 +0400

    singupservlet

--- src/main/java/servlets/SignupServlet.java
+++ src/main/java/servlets/SignupServlet.java
@@ -5,10 +5,11 @@ import classes.UserDAO;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+@WebServlet("/SignupServlet")
 public class SignupServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
