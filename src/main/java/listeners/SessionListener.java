a4028d71eda9356f11fdc42a7ddcf610065bca69
icosahedron31 <tdola23@freeuni.edu.ge>
2025-06-18 23:33:05 +0400

    nothing

--- src/main/java/listeners/SessionListener.java
+++ src/main/java/listeners/SessionListener.java
@@ -1,4 +1,16 @@
-package main.java.listeners;
+package listeners;
 
-public class SessionListener {
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+@WebListener
+public class SessionListener implements HttpSessionListener {
+    public void sessionCreated(HttpSessionEvent event) {
+        HttpSession session = event.getSession();
+
+    }
+    public void sessionDestroyed(HttpSessionEvent event) {}
 }
