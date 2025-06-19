4a9e4c73c90fefbcc49feee38c873983ecf78195
icosahedron31 <tdola23@freeuni.edu.ge>
2025-06-19 22:45:53 +0400

    added FriendRequestResponse

---
+++ src/main/java/servlets/FriendRequestReponse.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import database.FriendRequestDAO;
+import database.FriendsDAO;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/FriendRequestResponse")
+public class FriendRequestReponse extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String sender = request.getParameter("sender");
+        String receiver = request.getParameter("receiver");
+        String status =  request.getParameter("status");
+        ServletContext context = getServletContext();
+        FriendsDAO friendsDAO = (FriendsDAO)context.getAttribute("friendsDAO");
+        FriendRequestDAO friendRequestDAO = (FriendRequestDAO)context.getAttribute("friendRequestDAO");
+        friendRequestDAO.removeRequest(sender, receiver);
+        if(status.equals("accept")){
+            friendsDAO.addFriends(sender, receiver);
+            friendsDAO.addFriends(receiver, sender);
+        }
+        String referer = request.getHeader("Referer");
+        response.sendRedirect(referer);
+    }
+}
