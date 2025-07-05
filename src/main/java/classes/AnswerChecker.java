7efda10e42d0727fd8bebd0c9824376b71b71555
dzidz1 <adzid23@freeuni.edu.ge>
2025-07-05 16:34:05 +0400

    Answer checking

---
+++ src/main/java/classes/AnswerChecker.java
@@ -0,0 +1,12 @@
+package classes;
+
+public interface AnswerChecker {
+    /**
+     * Checks if the provided answer is correct for the given question.
+     *
+     * @param questionID the ID of the question to check
+     * @param userAnswer the user's answer input
+     * @return true if the answer is correct, false otherwise
+     */
+    boolean isCorrect(String questionID, String userAnswer);
+}
