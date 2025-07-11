848bbb919935eda326b327af9211585e2c4e2c02
icosahedron31 <tdola23@freeuni.edu.ge>
2025-07-11 14:05:57 +0400

    added challengeMail

---
+++ src/main/java/classes/mail/ChallengeMailSender.java
@@ -0,0 +1,14 @@
+package classes.mail;
+
+import classes.social.Challenge;
+
+import java.sql.Timestamp;
+
+public class ChallengeMailSender {
+    private Challenge x;
+    private Timestamp sendDate;
+    public ChallengeMailSender(Challenge x, Timestamp sendDate) {
+
+    }
+
+}
